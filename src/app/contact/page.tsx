5b33243725fe2a0e64da8adbe7bc1323343c7509
Igor Dulovic <igorx24@gmail.com>
2024-12-08 10:44:42 +0100

    setting global padding

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,11 +5,11 @@ import { useRouter } from "next/navigation";
 const ContactUs = () => {
   const router = useRouter();
   return (
-    <div>
+    <div className="flex justify-between">
       <h1>About Us</h1>
       <button
         onClick={() => router.push("/")}
-        className="bg-blue-500 text-white p-2 rounded-md"
+        className="bg-blue-500 text-white p-1 rounded-md"
       >
         Go to Home
       </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Company Intro</h1>;
+  return <h1>Company Info</h1>;
 }
